b4716bc7160b90a1eaa19438de7d8cc6aaaadc26
Ayesha Siddiqa <145910115+Aisha60@users.noreply.github.com>
2025-11-09 21:32:40 +0500

    Delete expressly directory

--- expressly/server/routes/practice.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { generatePracticeTask } from "../controllers/practiceController.js";
-
-const router = express.Router();
-
-// POST /api/practice/generate-task
-router.post("/generate-task", generatePracticeTask);
-
-export default router;
